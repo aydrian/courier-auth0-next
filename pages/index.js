7f6543561a4ec0c8ca83078a7ed4a95126538369
Aydrian Howard <aydrian@gmail.com>
2021-07-07 16:50:06 -0400

    added auth0 login

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import { useUser } from "@auth0/nextjs-auth0";
 
 export default function Home() {
+  const { user, error, isLoading } = useUser();
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
   return (
     <div className="container">
       <Head>
@@ -11,13 +16,21 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header
+          title={`Welcome ${user ? user.name + ", " : ""} to my Courier app!`}
+        />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
+          <br />
+          {!user ? (
+            <a href="/api/auth/login">Login</a>
+          ) : (
+            <a href="/api/auth/logout">Logout</a>
+          )}
         </p>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
